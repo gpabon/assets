b16951a7d6ac4026134d79c21a5bd0092a9abb12
Gustavo Pabon <gustavopabon@hotmail.com>
2019-05-10 22:02:56 -0500

    evaluate pypfopt

--- bvc/evaluate_pypfopt.py
+++ bvc/evaluate_pypfopt.py
@@ -1,14 +1,59 @@
 import mpt
 import pandas as pd
 import numpy as np
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models, expected_returns
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     prices = pd.read_csv('data/bvc/stocks.csv', index_col=0)
+    
+    mu = expected_returns.mean_historical_return(prices)
+    S = risk_models.sample_cov(prices)
+    ef = EfficientFrontier(mu, S)
+    weights = ef.max_sharpe()
+    wi_opt_lst = []
+    for col in prices.columns:
+        wi_opt_lst.append(weights[col])
+    wi_opt = np.array(wi_opt_lst)
+
     returns = mpt.daily_pct_returns(prices)
     ERi = mpt.return_means(returns)
-    wi = mpt.random_wi(len(ERi))
-    ERp = mpt.portfolio_expected_return(wi, ERi)
-    sigma2p = mpt.portfolio_variance(wi, returns)
+    #wi = mpt.random_wi(len(ERi))
+    ERp = mpt.portfolio_expected_return(wi_opt, ERi)
+    sigma2p = mpt.portfolio_variance(wi_opt, returns)
     sigmap = np.sqrt(sigma2p)
     rf = .027 / (20*12)
-    print(mpt.sharpe_ratio(ERp, rf, sigmap))
+    optimal_sharpe = mpt.sharpe_ratio(ERp, rf, sigmap)
+    print(optimal_sharpe)
+
+    pf_opt_x = [sigmap]
+    pf_opt_y = [ERp]
+    ef_x = [0, sigmap]
+    ef_y = [rf, ERp]
+    pf_x = []
+    pf_y = []
+    for i in range(10000):
+        if i % 100 == 0:
+            print('.', end=' ', flush=True)
+        wi = mpt.random_wi(len(ERi))
+        ERp = mpt.portfolio_expected_return(wi, ERi)
+        pf_y.append(ERp)
+        sigma2p = mpt.portfolio_variance(wi, returns)
+        sigmap = np.sqrt(sigma2p)
+        pf_x.append(sigmap)
+        sharpe = mpt.sharpe_ratio(ERp, rf, sigmap)
+        if sharpe > optimal_sharpe:
+            print(sharpe)
+            print(wi)
+    print()
+    plt.scatter(pf_x, pf_y)
+    plt.plot(ef_x, ef_y)
+    plt.scatter(pf_opt_x, pf_opt_y, c='r')
+    plt.savefig('log/foo.png', bbox_inches='tight')
+
+    #wi_opt[np.abs(wi_opt) < 1e-3] = 0
+    wi_opt = np.round(wi_opt, 2)
+    print(wi_opt)
+    print(sum(wi_opt))
--- mpt.py
+++ mpt.py
@@ -25,6 +25,20 @@ def portfolio_variance(wi, returns):
 def sharpe_ratio(ERp, rf, sigmap):
     return((ERp-rf)/sigmap)
 
+def clean_wi(wi, decimals):
+    return(np.round(wi, decimals))
+
+def min_size(wi, prices):
+    last_date = max(prices.index)
+    last_prices = prices.loc[last_date,:]
+    possible_vols = last_prices / wi
+    possible_vols = possible_vols.replace(np.inf, 0)
+    min_vol = max(possible_vols)
+    vols = wi * min_vol
+    num_shares = np.round(vols / last_prices,0)
+    vols_adjusted = last_prices*num_shares
+    return(num_shares, np.round(vols_adjusted.sum(),0))
+
 if __name__ == '__main__':
     prices = pd.read_csv('data/bvc/stocks.csv', index_col=0)
     returns = daily_pct_returns(prices)
@@ -35,3 +49,6 @@ if __name__ == '__main__':
     sigmap = np.sqrt(sigma2p)
     rf = .027 / (20*12)
     print(sharpe_ratio(ERp, rf, sigmap))
+    wic = clean_wi(wi,2)
+    print(wic)
+    print(min_size(wic, prices))
